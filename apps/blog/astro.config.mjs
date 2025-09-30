6d51b9e9f16bdab85bc92ad846058b3146a0d403
kozan26 <kozan26@gmail.com>
2025-09-30 16:35:38 +0300

    Delete apps/blog directory

--- apps/blog/astro.config.mjs
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://blog.simscent.com', // set your blog subdomain
-  scopedStyleStrategy: 'where'       // safer CSS scoping
-});
