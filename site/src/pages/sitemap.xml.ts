45bb1b6d91156c372e89881e30334a68f0d1b113
kozan26 <kozan26@gmail.com>
2025-10-01 22:57:54 +0300

    Update sitemap.xml.ts

--- site/src/pages/sitemap.xml.ts
+++ site/src/pages/sitemap.xml.ts
@@ -1,12 +1,11 @@
 export const prerender = true;
 
 export async function GET() {
-  // İstersen ana sitenin sayfalarını burada dinamik toplayabilirsin.
-  // Basit bir index’ten link vermek istiyorsan redirect de yapabiliriz (Seçenek B).
+  // İstersen sadece index’e yönlendir:
   const body = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url><loc>https://www.simscent.com/</loc></url>
-  <url><loc>https://www.simscent.com/blog</loc></url>
-</urlset>`;
-  return new Response(body, { headers: { 'Content-Type': 'application/xml; charset=utf-8' }});
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap><loc>https://www.simscent.com/sitemap.xml</loc></sitemap>
+  <sitemap><loc>https://blog.simscent.com/sitemap-index.xml</loc></sitemap>
+</sitemapindex>`;
+  return new Response(body, { headers: { 'Content-Type': 'application/xml; charset=utf-8' } });
 }
