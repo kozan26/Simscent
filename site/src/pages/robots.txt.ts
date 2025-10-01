3e9f02856e8afe90ab612a68bd61686e3b314d97
kozan26 <kozan26@gmail.com>
2025-10-01 22:54:57 +0300

    Create robots.txt.ts

---
+++ site/src/pages/robots.txt.ts
@@ -0,0 +1,10 @@
+export const prerender = true;
+
+export async function GET() {
+  const body = `User-agent: *
+Allow: /
+
+Sitemap: https://www.simscent.com/sitemap-index.xml
+`;
+  return new Response(body, { headers: { 'Content-Type': 'text/plain; charset=utf-8' } });
+}
